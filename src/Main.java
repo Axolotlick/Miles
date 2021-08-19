04f4d5148b5cf05410822e67d2b722ba6656e481
Viktoria Kurochkina <gekkon.kun@gmail.com>
2021-08-19 18:08:38 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 7963;
+        int amount = 20;
+        int miles = price / amount;
+
+        System.out.println(miles);
+    }
+}
